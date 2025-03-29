88338a79c9188f076df444eb432de7bb17e21792
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 12:35:51 -0300

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
-import { serverNextClientArchitecture } from "@caucolum/next-client-architecture"
 
 export default function Home() {
-  const response = serverNextClientArchitecture.requestc();
-  console.log(response);
   return <div>
     hello home
   </div>
